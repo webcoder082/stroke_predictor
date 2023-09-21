ec6381734f99dc5342504de563b454a506007857
webcoder082 <137896614+webcoder082@users.noreply.github.com>
2023-09-21 20:08:02 +0530

    Update frontendcode.py

--- frontendcode.py
+++ frontendcode.py
@@ -4,15 +4,15 @@ def main():
     st.title("Stroke Prediction a product by Intro-Me")
     gender=st.text_input("Gender")
     
-    age=st.text_input("age")
-    hypertension=st.text_input("Hypertension")
-    heartproblem=st.text_input("heartproblem")
-    married_or_not=st.text_input("married_or_not")
-    private_or_self_or_gov_job=st.text_input("private_or_self_or_gov _job")
-    urban_or_rural=st.text_input("urban_or_rural")
+    age=st.text_input("Enter Age")
+    hypertension=st.radio("Do you have blood pressure",["Yes","No"])
+    heartproblem=st.radio("Do you have Heart problem",["Yes","No"])
+    married_or_not=st.radio("Are you married",["Yes","No"])
+    private_or_self_or_gov_job=st.radio("IN Which sector you are working",["GOVERNMENT","PRIVATE","SELF-EMPLOYEE])
+    urban_or_rural=st.radio("which area ypou are living",["Rural","Urban"])
     glucose_levels=st.text_input("glucose_levels")
     BMI=st.text_input("BMI")
-    smoker_or_not=st.text_input("smoker_or_not")
+    smoker_or_not=st.radio("Are you a smoker",["Yes","No"])
     if st.button('Predict'):
         st.success("youclicked ")
 
