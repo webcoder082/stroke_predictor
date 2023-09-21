9314a3e6522ad5b1aff160e65802ac40494ac04c
webcoder082 <137896614+webcoder082@users.noreply.github.com>
2023-09-21 20:08:42 +0530

    Update frontendcode.py

--- frontendcode.py
+++ frontendcode.py
@@ -8,7 +8,7 @@ def main():
     hypertension=st.radio("Do you have blood pressure",["Yes","No"])
     heartproblem=st.radio("Do you have Heart problem",["Yes","No"])
     married_or_not=st.radio("Are you married",["Yes","No"])
-    private_or_self_or_gov_job=st.radio("IN Which sector you are working",["GOVERNMENT","PRIVATE","SELF-EMPLOYEE])
+    private_or_self_or_gov_job=st.radio("IN Which sector you are working",["GOVERNMENT","PRIVATE","SELF-EMPLOYEE"])
     urban_or_rural=st.radio("which area ypou are living",["Rural","Urban"])
     glucose_levels=st.text_input("glucose_levels")
     BMI=st.text_input("BMI")
