79288a369b579d551acfdd4ee5992f54d356138c
webcoder082 <137896614+webcoder082@users.noreply.github.com>
2023-09-29 20:53:54 +0530

    Update frontendcode.py

--- frontendcode.py
+++ frontendcode.py
@@ -7,6 +7,9 @@ def main():
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            .viewerBadge_link__qRIco{
+            visibility: hidden;
+            }
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
